4a66ead4d6d32dac69dfde084f6588dd7ed0409d
AnDreW <andrew21ls@yandex.ru>
2020-07-14 21:00:32 +0300

    Добавьте файлы проекта.

---
+++ InstaBot/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace InstaBot
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitialForm();
+        }
+
+        private void InitialForm()
+        {
+            SourceP.Text = @"E:\Browser_Downloads\bmw";
+            DestinyP.Text = @"E:\Browser_Downloads\bmw\tmp";
+            InstName1.Text = "vasylko.kuznetc";
+            InstName2.Text = "i2va22020";
+            InstPass1.Text = "qwpogh1290_153";
+            InstPass2.Text = "polo12378";
+            Count.Text = "5";
+        }
+         
+ 
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyPost post = new MyPost(SourceP.Text,DestinyP.Text,InstName1.Text,InstPass1.Text,Count.Text);
+
+            if (post.CopyFilesIntoAccont())
+                System.Windows.MessageBox.Show("Succes","Information");
+
+            // Application.Current.Shutdown();
+            //   var direct = Directory.GetFiles()
+        }
+
+        private void SourcePath_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+ 
+    private void DestinyPath_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ InstaBot/MyPost.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaBot
+{
+    using System;
+
+    public class MyPost
+    {
+        public int count;
+        public string sourcePath, destinyPath, userName, userPassword;
+
+        public MyPost(string _sourcePath, string _destinyPath, string _name, string _pass, string _count)
+        {
+            count = Convert.ToInt32(_count);
+            sourcePath = _sourcePath;
+            destinyPath = _destinyPath;
+            userName = _name;
+            userPassword = _pass;
+
+        }
+
+        public bool CopyFilesIntoAccont()
+        {
+            bool res = true;
+            return res;
+        }
+
+    }
+
+}
